bf5bce1df04ab0c2f969e9c26c814a39d4182d84
Akshitkt001 <137613929+Akshitkt001@users.noreply.github.com>
2023-06-27 12:26:56 +0530

    Add files via upload

---
+++ public_html/connection.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$username = "id18031731_atkumar0802";
+$password = "Akshitkumar@0099";
+$server = 'localhost';
+$db = 'id18031731_education';
+
+$con = mysqli_connect($server,$username,$password,$db);
+
+if($con){
+    //echo "Connection Successful";
+
+    
+}
+else{
+   // echo "No Connection";
+}
+
+
+?>
